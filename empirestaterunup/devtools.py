6f4cc923b0f32bde2d6d8c53fa97ad3960afef3c
Jose Vicente Nunez <josevnz@kodegeek.com>
2026-01-31 22:09:33 -0500

    Data is clean, new tool to help with that

--- empirestaterunup/devtools.py
+++ empirestaterunup/devtools.py
@@ -2,19 +2,18 @@ from pathlib import Path
 
 import pandas
 
-from empirestaterunup.data import load_location_lookup, location_lookup
+from empirestaterunup.data import load_location_lookup, location_lookup, RaceFields
 
 
 def enrich_race_results(
-    location_lookup_file: Path,
-    race_results_file: Path
+        location_lookup_file: Path,
+        race_results_file: Path
 ) -> list[dict]:
     ll = load_location_lookup(data_file=location_lookup_file)
     race_results = pandas.read_json(race_results_file, lines=True, encoding='utf-8').to_dict(orient='records')
     for result in race_results:
-        if result['country'] == '':
-            locality = result['race_results']
+        if result[RaceFields.COUNTRY.value] == '':
+            locality = result[RaceFields.CITY.value].lower()
             two_letter_code = location_lookup(lookup_data=ll, locality=locality)
-            result['country'] = two_letter_code
+            result[RaceFields.COUNTRY.value] = two_letter_code
     return race_results
-
--- test/test_devtools.py
+++ test/test_devtools.py
@@ -1,9 +1,18 @@
 import unittest
+from pathlib import Path
+
+from empirestaterunup.data import LOCATION_DETAILS, RaceFields
+from empirestaterunup.devtools import enrich_race_results
+
+TEST_RACE_FILE = Path(__file__).parent.joinpath("results-raw-2025.jsonl")
 
 
 class DevtoolsTestCase(unittest.TestCase):
     def test_enrich_race_results(self):
-        self.assertEqual(True, False)  # add assertion here
+        rr = enrich_race_results(race_results_file=TEST_RACE_FILE, location_lookup_file=LOCATION_DETAILS)
+        self.assertIsNotNone(rr)
+        for rs in rr:
+            self.assertNotEquals('', rs[RaceFields.COUNTRY.value])
 
 
 if __name__ == '__main__':
